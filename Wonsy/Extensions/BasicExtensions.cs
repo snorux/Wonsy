f25d7bf704d399dff2dbf0a2103e7b37c5f30a90
Snowy <34697265+snooooowy@users.noreply.github.com>
2023-09-18 23:20:08 +0800

    Update packages, misc changes

--- Wonsy/Extensions/BasicExtensions.cs
+++ Wonsy/Extensions/BasicExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class BasicExtensions
     {
-        public static string FormattedValue(this IUser user, bool withId = false) => withId ? $"{user.Username}#{user.Discriminator} [{user.Id}]" : $"{user.Username}#{user.Discriminator}";
+        public static string FormattedValue(this IUser user, bool withId = false) => withId ? $"{user.Username}{(user.Discriminator == "0000" ? "" : $"#{user.Discriminator}")} [{user.Id}]" : $"{user.Username}{(user.Discriminator == "0000" ? "" : $"#{user.Discriminator}")}";
 
         public static string FormattedValue(this IGuild guild, bool withId = false) => guild == null ? "PRIVATE" : withId ? $"{guild.Name} [{guild.Id}]" : $"{guild.Name}";
 
